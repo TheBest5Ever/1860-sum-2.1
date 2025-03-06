dabfbf2a696c02307a0d9647936825bdaaba1efe
TheBest5Ever <ll23rb@leeds.ac.uk>
2025-03-06 18:48:44 +0000

    jjjjj

--- Xor.asm
+++ Xor.asm
@@ -2,17 +2,7 @@
     D=M         // D = R0
 
     @R1
-    D=D&M       // D = common bits between R0 and R1
+    D=D^M       // D = R0 ^ R1  (XOR)
 
     @R2
-    M=D         // Store R0 & R1 temporarily in R2
-
-    @R0
-    D=M         // D = R0
-
-    @R1
-    D=D|M       // D = All bits that are 1 between R0 and R1
-
-    @R2
-    D=D-M       // D = R0 or R1 - R0 and R1 (xor)
-
+    M=D         // Store the result in R2
