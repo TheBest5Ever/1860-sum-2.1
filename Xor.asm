614984f10012db48990d3b07f64cf3fcba68e3a0
TheBest5Ever <ll23rb@leeds.ac.uk>
2025-03-06 19:30:33 +0000

    xzzx# Please enter the commit message for your changes. Lines starting

--- Xor.asm
+++ Xor.asm
@@ -1,14 +1,19 @@
     @R0
     D=M        
+    
     @R1
-    D=D&M       
-    @R2
-    M=D         
-    @R0
-    D=M       
-    @R1
-    D=D|M       
-    D=D-M     
+    D=D&M      
 
     @R2
-    M=D       
+    M=D        
+
+    @R0
+    D=M         
+
+    @R1
+    D=D|M       
+
+    @R2
+    D=D-M       
+    M=D
+ 
